66e5e452e2e0172d0531d82605c6e917b738777b
CollinsJ25 <CollinsJ25@greenhill.org>
2022-06-21 08:26:56 -0500

    update

--- PygameFiles/Images/Testing.py
+++ PygameFiles/Images/Testing.py
@@ -109,8 +109,7 @@ def mainMenu():
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 
-               pygame.quit()
-               sys.exit 
+                mainMenu()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mousePos = pygame.mouse.get_pos()
@@ -133,18 +132,107 @@ def mainMenu():
                     exit()
 
 def test():
-    global bg
+    global bg, walkLeft, walkRight, height, walkCount
+    win = pygame.display.set_mode((700,700))
+    bg = pygame.image.load('bgSmaller.jpg')
+    walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
+    walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
     screen.blit(bg, (0,0))
-    pygame.display.update
+    pygame.display.flip()
     platform = pygame.Rect(WIDTH//4-WIDTH//12, HEIGHT//4, WIDTH//6, HEIGHT//14)
     pygame.draw.rect(screen, colors.get("blue"), platform)
+    print ('HERE')
+    x = 50
+    y = 600
+    width = 40
+    height = 60
+    vel = 5
+
+    clock = pygame.time.Clock()
+
+    isJump = False
+    jumpCount = 10
+
+    left = False
+    right = False
+    walkCount = 0
+
+    def Play():
+        global walkCount
+        
+        screen.blit(bg, (0,0))  
+        if walkCount + 1 >= 27:
+            walkCount = 0
+            
+        if left:  
+            screen.blit(walkLeft[walkCount//3], (x,y))
+            walkCount += 1                          
+        elif right:
+            win.blit(walkRight[walkCount//3], (x,y))
+            walkCount += 1
+        else:
+            win.blit(char, (x, y))
+            walkCount = 0
+            
+        pygame.display.update() 
+        
+
+
+    run = True
+
+    while run:
+        clock.tick(27)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_LEFT] and x > vel: 
+            x -= vel
+            left = True
+            right = False
+            Play()
+
+        elif keys[pygame.K_RIGHT] and x < 500 - vel - width:  
+            x += vel
+            left = False
+            right = True
+            Play()
+        else: 
+            left = False
+            right = False
+            walkCount = 0
+            Play()
+
+        if not(isJump):
+            if keys[pygame.K_SPACE]:
+                isJump = True
+                left = False
+                right = False
+                walkCount = 0
+                Play()
+        else:
+            if jumpCount >= -10:
+                y -= (jumpCount * abs(jumpCount)) * 0.5
+                jumpCount -= 1
+                Play()
+            else: 
+                jumpCount = 10
+                isJump = False
+                Play()
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                mainMenu()
+    
 
 def Easy():
     global height, walkCount
     import pygame
     pygame.init()
 
-    win = pygame.display.set_mode((500,480))
+    #win = pygame.display.set_mode((500,480))
     pygame.image.load('bgSmaller.jpg')
 
     walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
@@ -170,18 +258,18 @@ def Easy():
     def Play():
         global walkCount
         
-        win.blit(bg, (0,0))  
+        screen.blit(bg, (0,0))  
         if walkCount + 1 >= 27:
             walkCount = 0
             
         if left:  
-            win.blit(walkLeft[walkCount//3], (x,y))
+            screen.blit(walkLeft[walkCount//3], (x,y))
             walkCount += 1                          
         elif right:
-            win.blit(walkRight[walkCount//3], (x,y))
+            screen.blit(walkRight[walkCount//3], (x,y))
             walkCount += 1
         else:
-            win.blit(char, (x, y))
+            screen.blit(char, (x, y))
             walkCount = 0
             
         pygame.display.update() 
@@ -384,10 +472,10 @@ def settings():
                     
                     pygame.draw.rect(screen, colors.get("purple"), Button_9)
                     textSurface=MENU_FONT.render(name, True, 'yellow')
-                   # screen.blit(textSurface, name(Button_9,mx+5, Button_9,my+5))
+                   
                     screen.blit(textSurface, Button_9)
                     pygame.display.flip()
-                    #screen.blit(name, Button_9)
+                    
                  
 
 
